f63697f6d99168acba9a5671dd4c3b6f4cf93dd9
semih <94394628+Semih1907temur@users.noreply.github.com>
2024-05-01 19:18:01 +0300

    Initial commit

---
+++ src/test/java/com/example/odevv/OdevvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.odevv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdevvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
